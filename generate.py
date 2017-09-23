87583b1f9473967bea2658deaf8d7b33a5943b82
Gil <gil707@gmail.com>
2017-09-23 12:51:13 +0300

    get invoice from json file

--- generate.py
+++ generate.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 import lzma
 from graphenebase.base58 import base58encode, base58decode
 from binascii import hexlify, unhexlify
 
-invoice = {
-    "to": "gurondex-cny",
-    "to_label": "GuronDEX",
-    "currency": "RUBLE",
-    "memo": "Invoice #2234",
-    "fee_id": "1.3.1325",
-    "line_items": [
-        {"label": "T-shirt (M) color:yellow #532", "quantity": 2, "price": "1.00"},
-        {"label": "Glasses (aviator)black SKU#3548", "quantity": 1, "price": "0.50"}
-    ],
-    "note": "Payment for order #533",
-    "callback": "https://market.gurondex.io/complete"
-}
+with open(sys.argv[1]) as inv:
+    invoice = json.load(inv)
 
 compressed = lzma.compress(bytes(json.dumps(invoice), 'utf-8'), format=lzma.FORMAT_ALONE)
 b58 = base58encode(hexlify(compressed).decode('utf-8'))
