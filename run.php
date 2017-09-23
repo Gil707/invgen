80b2b05684cb4cc33ad8ce7c6c07082e9c6e4a58
Gil <gil707@gmail.com>
2017-09-23 22:57:44 +0300

    change invoice folder, add QR-code

--- run.php
+++ run.php
@@ -3,24 +3,33 @@
 
 
     <form action="run.php" method="post" style="font-size: 12px; font-family: 'Lucida Console'">
-        Invoice: <input type="text" name="filename"><br><br>
+        Invoice:
+        <select name="invoice">
+            <option value="gndex_inv0000001">Invoice #1</option>
+            <option value="gndex_inv0000002">Invoice #2</option>
+            <option value="gndex_inv0000003">Invoice #3</option>
+        </select>
+
         <input type="submit" value="Generate" style="font-family: 'Lucida Console'; font-size: 12px">
     </form>
 
 <?php
 
 
-if (isset($_POST['filename'])) {
+if (isset($_POST['invoice'])) {
 
-    $command = escapeshellcmd('python3 generate.py ' . $_POST['filename'] . '.json');
+    $command = escapeshellcmd('python3 generate.py invoices/' . $_POST['invoice'] . '.json');
     $output = shell_exec($command);
 
-    $inv = fopen($_POST['filename'].'.json', 'r') or die('Unable to open file!');
-    echo "<hr>";
-    echo "<pre>" . fread($inv, filesize($_POST['filename'].'.json')) . "</pre><br>";
-    fclose($inv);
+//    $inv = fopen($_POST['filename'].'.json', 'r') or die('Unable to open file!');
 
-    echo "Your <a href='" . $output . "'>link</a>";
+    echo "<b>Invoice: ".$_POST['invoice']."</b><br>";
+    echo "<img src=\"https://chart.googleapis.com/chart?chs=400x400&chld=Q&cht=qr&chl=".$output."\">";
+    echo "<br><br>Your <a href='" . $output . "'>link</a>";
+
+    echo "<hr style='color: grey'>";
+//    echo "<pre style='color: grey'>" . fread($inv, filesize($_POST['filename'].'.json')) . "</pre><br>";
+//    fclose($inv);
 }
 
 ?>
